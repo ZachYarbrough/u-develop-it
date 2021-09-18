3357b05b10337044e7aae43ea1ecde17434d2886
Zach Yarbrough <zachyarbro@gmail.com>
2021-09-18 12:44:17 -0500

    added .env file to protect personal data

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection(
     {
-        host: 'localhost',
-        user: 'root',
-        password: '1luvBilb0',
-        database: 'election'
+        host: process.env.DB_HOST,    
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME
     }
 );
 
